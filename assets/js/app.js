4135c5e6ee6680aea1b65caa760163e5cd1da6a2
HinataKikuchi <haiao4041@gmail.com>
2022-07-05 22:56:05 +0900

    ✨:ポップアップとアイコンを追加（New Feature）

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,22 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   }
 ).addTo(map);
 
+//一時避難所のアイコン追加
+var tmpEvacuationParkIcon = L.icon({
+  iconUrl: './icon/tmporaly_evacuation_site.png', // アイコン画像のURL
+  iconSize:     [40, 40], // アイコンのサイズをピクセルで指定
+  iconAnchor:   [25, 50], // アイコンの先端の座標（右上からの相対座標）
+  popupAnchor:  [0, -50] // ポップアップが開く点の座標（iconAnchorが基準）
+});
+
+//広域避難場所のアイコン追加
+var wideEvacuationParkIcon = L.icon({
+  iconUrl: './icon/wide_area_evacuation_site.png', // アイコン画像のURL
+  iconSize:     [40, 40], // アイコンのサイズをピクセルで指定
+  iconAnchor:   [25, 50], // アイコンの先端の座標（右上からの相対座標）
+  popupAnchor:  [0, -50] // ポップアップが開く点の座標（iconAnchorが基準）
+});
+
 // leafletに追加するリストを定義
 let parkLocationList = [];
 
@@ -24,21 +40,46 @@ fetch(tmpEvacuationParkUrl)
   .then(response => response.json())
   // GeoJSONを地図に追加する
   .then(data => {
-    parkLocationList[parkLocationList.length] = L.geoJSON(data);
-    console.log('Could get tmp json');
+    parkLocationList.push(L.geoJSON(data,{
+       // カスタムアイコンを設定する
+      pointToLayer: function(feature, latlng) {
+        return L.marker(latlng, {icon: tmpEvacuationParkIcon});
+      },
+      // ポップアップを表示する
+      onEachFeature: function (feature, layer) {
+        // 建物の名前を取り出す(改行<br>)
+        let name = feature.properties.title + '<br>';
+        // ポップアップに名前を表示する
+        layer.bindPopup(name);
+      }
+    }));
+    console.log('could get tmp json',data);
+    
+    //広域避難所のデータ
+    var wideEvacuationParkUrl = './data/geojson_data/wide_park.geojson';
+    fetch(wideEvacuationParkUrl)
+      .then(response => response.json())
+      // GeoJSONを地図に追加する
+      .then(data => {
+        parkLocationList.push(L.geoJSON(data, {
+          // カスタムアイコンを設定する
+          pointToLayer: function(feature, latlng) {
+          return L.marker(latlng, {icon: wideEvacuationParkIcon});
+        },
+          // ポップアップを表示する
+          onEachFeature: function(feature, layer){
+          // 建物の名前を取り出す(改行<br>)
+          let name = feature.properties.title+'<br>';
+          // ポップアップに名前を表示する
+          layer.bindPopup(name);
+      }
+      }));
+        console.log('Could get wide json',L.geoJSON(data));
+        let group = L.layerGroup(parkLocationList);
+        console.log(group);
+        group.addTo(map);
+      });
   });
 
-//広域避難所のデータ
-var wideEvacuationParkUrl = './data/geojson_data/wide_park.geojson';
-fetch(tmpEvacuationParkUrl)
-  .then(response => response.json())
-  // GeoJSONを地図に追加する
-  .then(data => {
-    parkLocationList[parkLocationList.length] = L.geoJSON(data);
-    console.log('Could get wide json');
-  });
 
-
-let group = L.layerGroup(parkLocationList);
-group.addTo(map);
 // L.geoJSON(data).addTo(map);
