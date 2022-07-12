6917ef9f6109abc83658590f6d7f664439a60fd3
HinataKikuchi <haiao4041@gmail.com>
2022-07-12 22:58:28 +0900

    🎨:情報の追加ー(Accessibility)

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,9 +41,30 @@ function getWidthParkLocation() {
   return fetch(url).then(response => response.json())
 }
 
-/*ここでpopupのスタイルを整形して表示したい*/
-function generatePopUpMessage(title, facility, imgURL) {
+const facilityList=['防災トイレ','ソーラー発電の公園灯','揚水ポンプ','防火水槽','応急給水槽','ヘリコプター臨時離着陸場','かまどベンチ','防災あずまや','防災備蓄庫'];
 
+/*ここでpopupのスタイルを整形して表示したい*/
+function generatePopUpMessage(feature) {
+
+  var message ='';
+  message += `<img src="${feature.properties.image_url}" class="leaflet-popup-thumbnail"/>`
+  message += '<ol class="mapExplanation"><li>名称</li>' + feature.properties.title;
+  message += '<li>住所</li>' + '栃木県宇都宮市' + feature.properties.city;
+  message += '<li>電話番号</li>' + feature.properties.Tel;
+  message += '<li>設備情報</li><ul class="pl-1">';
+  message += facilityList.map(function (value) {
+    // .jsonのfacilityの中身をforで回す
+    var resultText
+    if (feature.properties[value]) {
+      resultText = '<li>⭕' + value + '</li>'
+    } else {
+      resultText = '<li>❌' + value + '</li>'
+    }
+    return resultText;
+  }).join('');
+  message += '</ul>'
+  // '<li><a href="">html</a></li><li><a href="">css</a></li><li><a href="">javacrip</a></li></ol></li><li><a href="">Site map</a></li><li><a href="">Mail</a></li></ol>';
+  return message
 }
 
 //定義された関数を呼び出すことでfetchをpromise.allがいっぺんに処理する
@@ -62,10 +83,10 @@ Promise.all([getTmpParkLocation(), getWidthParkLocation()]).then(values => {
       },
       // ポップアップを表示する
       onEachFeature: function (feature, layer) {
-        // 建物の名前を取り出す(改行<br>)
-        let name = feature.properties.title + '<br>';
-        // ポップアップに名前を表示する
-        layer.bindPopup(name);
+        // 建物の情報を表示用に成形
+        var featureExplanation = generatePopUpMessage(feature);
+        // 表示に挿入
+        layer.bindPopup(featureExplanation);
       }
     }),
 
@@ -77,10 +98,10 @@ Promise.all([getTmpParkLocation(), getWidthParkLocation()]).then(values => {
       },
       // ポップアップを表示する
       onEachFeature: function (feature, layer) {
-        // 建物の名前を取り出す(改行<br>)
-        let name = feature.properties.title + '<br>';
-        // ポップアップに名前を表示する
-        layer.bindPopup(name);
+        // 建物の情報を表示用に成形
+        var featureExplanation = generatePopUpMessage(feature);
+        // 表示に挿入
+        layer.bindPopup(featureExplanation);
       }
     }),
   ];
